b1ffa4c64b7056a40eef73054d223be276a8b160
Shanmukesh <shanmukeshbonala4622@gmail.com>
2025-09-16 18:00:15 +0000

    can you add Padding like i feel all the containers are at Edge of the pa

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,5 +2,9 @@
 import { Suspense } from 'react';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <Suspense>{children}</Suspense>;
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+        <Suspense>{children}</Suspense>
+    </div>
+    );
 }
