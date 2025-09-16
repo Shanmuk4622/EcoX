39de1b6fa72e4f3ee40e439e8a0ed688358ef116
Shanmukesh <shanmukeshbonala4622@gmail.com>
2025-09-16 19:24:28 +0000

    no, revert this step

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,31 +1,9 @@
 
-import { Header } from '@/components/header';
-import { MainNav } from '@/components/main-nav';
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarInset,
-  SidebarProvider,
-} from '@/components/ui/sidebar';
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarContent>
-          <MainNav />
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset>
-        <div className="flex h-full flex-col">
-          <Header />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <main>{children}</main>;
 }
